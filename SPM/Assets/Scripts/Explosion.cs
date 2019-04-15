83e2e88e977d20d938c5d530c1060fb3bb228e43
Bhaalder <37620605+Bhaalder@users.noreply.github.com>
2019-04-15 16:11:48 +0200

    updating some scripts and prefabs

--- SPM/Assets/Scripts/Explosion.cs
+++ SPM/Assets/Scripts/Explosion.cs
@@ -5,25 +5,25 @@ using UnityEngine;
 public class Explosion : MonoBehaviour
 {
     public float explosionRadius;
-
-    //public GameObject explosionEffect;
-
+    public GameObject explosionEffect;
+    
     public void Explode(float explosionForce, float damage) {
-
-        //Instantiate(explosionEffect, transform.position, transform.rotation);
-
         Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
 
-        foreach (Collider nearbyObject in colliders) {
-            if (nearbyObject.gameObject.layer == 9) {
+        foreach (Collider nearbyObject in colliders)
+        {
+            if (nearbyObject.gameObject.layer == 9)
+            {
                 nearbyObject.transform.GetComponent<EnemyController>().TakeDamage(damage);
             }
             Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
-            if (rb != null) {
+            if (rb != null)
+            {
                 rb.AddExplosionForce(explosionForce, transform.position, explosionRadius);
             }
         }
-
+        Instantiate(explosionEffect, transform.position, Quaternion.identity);
         Destroy(gameObject);
+        
     }
 }
