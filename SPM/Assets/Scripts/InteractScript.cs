d8d84c76a9bc7ca264ab9dbbff3b0a74199b1daf
teoalagic <teoalagicenglund@hotmail.com>
2019-04-20 01:03:35 +0200

    ändrat i lvl1 + ändrat switch scriptet

--- SPM/Assets/Scripts/InteractScript.cs
+++ SPM/Assets/Scripts/InteractScript.cs
@@ -7,10 +7,12 @@ public class InteractScript : MonoBehaviour
     public GameObject theObject;
     private Renderer rend1;
     private Collider coll1;
+    private bool isOpen;
     void Start()
     {
         coll1 = theObject.GetComponent<Collider>();
         rend1 = theObject.GetComponent<Renderer>();
+        isOpen = false;
     }
 
     // Update is called once per frame
@@ -29,10 +31,17 @@ public class InteractScript : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
+        if (other.gameObject.CompareTag("Player") && Input.GetKeyDown(KeyCode.E) && !isOpen)
         {
             coll1.enabled = false;
             rend1.enabled = false;
+            isOpen = true;
+        }else if (other.gameObject.CompareTag("Player") && Input.GetKeyDown(KeyCode.E) && isOpen)
+        {
+            coll1.enabled = true;
+            rend1.enabled = true;
+            isOpen = false;
         }
+        
     }
 }
