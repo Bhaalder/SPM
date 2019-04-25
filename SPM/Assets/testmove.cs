4c437c1bb2a763be3f0c07ad558158df30854569
teoalagic <teoalagicenglund@hotmail.com>
2019-04-25 17:26:32 +0200

    123

---
+++ SPM/Assets/testmove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testmove : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        transform.Translate(speed * moveHorizontal * Time.deltaTime, 0f, speed * moveVertical * Time.deltaTime);
+    }
+}
