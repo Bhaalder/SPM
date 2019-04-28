dffc5a204e509657425e3817baae1431e36eb857
Bhaalder <37620605+Bhaalder@users.noreply.github.com>
2019-04-29 01:01:52 +0200

    sdf

--- SPM/Assets/Scripts/WeaponController.cs
+++ SPM/Assets/Scripts/WeaponController.cs
@@ -25,19 +25,19 @@ public class WeaponController : MonoBehaviour
 
     public BaseWeapon GetRifle() {
         //////////////////////////////////Name____dmg_range_FR_reload_Impact_spr_aic_maic_totalammoleft
-        BaseWeapon rifle = new BaseWeapon("Rifle", 10, 100, 10f, 0.6f, 0.1f, 15, 50, 50, 150, null, null, null);
+        BaseWeapon rifle = new BaseWeapon("Rifle", 10, 100, 10f, 0.6f, 0.1f, 15, 50, 50, 500, null, null, null);
         return rifle;
     }
 
     public BaseWeapon GetShotgun() {
         /////////////////////////////////////Name____dmg_range_FR_reload_Impact_spr_aic_maic_totalammoleft
-        BaseWeapon shotgun = new BaseWeapon("Shotgun", 30, 18, 2f, 0.5f, 0.1f, 30, 5, 5, 20, null, null, null);
+        BaseWeapon shotgun = new BaseWeapon("Shotgun", 30, 18, 2f, 0.5f, 0.1f, 30, 5, 5, 200, null, null, null);
         return shotgun;
     }
 
     public ProjectileWeapon GetRocketLauncher() {
         /////////////////////////////////////////////////////////////////Name____dmg_range_FR_reload_Impact_spr_aic_maic_totalammoleft
-        ProjectileWeapon rocketLauncher = new ProjectileWeapon("Rocket Launcher", 30, 100, 1f, 0.3f, 0.01f, 20, 10, 5, 5, 10, rocketLaucherProjectileGO, null, null, null);
+        ProjectileWeapon rocketLauncher = new ProjectileWeapon("Rocket Launcher", 30, 100, 1f, 0.3f, 0.01f, 20, 10, 5, 5, 50, rocketLaucherProjectileGO, null, null, null);
         return rocketLauncher;
     }
 
