6dc28853ed347ee43113b9afc12a1b1cc8be2d46
WillCheung <37656199+WillCheung@users.noreply.github.com>
2019-04-30 11:36:19 +0200

    Färgkoder och knappfeedback

--- SPM/Assets/Scripts/OpenDoor.cs
+++ SPM/Assets/Scripts/OpenDoor.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class OpenDoor : MonoBehaviour
 {
     public GameObject theObject;
+    public GameObject button;
 
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.CompareTag("Player") && GameController.Instance.playerIsInteracting)
         {
             theObject.SetActive(!theObject.activeSelf);
+            button.SetActive(!button.activeSelf);
         }        
     }
 }
