dbd27adedc55302783b6e0a5fb750563bca3233c
WillCheung <37656199+WillCheung@users.noreply.github.com>
2019-05-06 21:29:27 +0200

    .,

---
+++ SPM/Assets/Scripts/Enemy/TriggerEnemySpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEnemySpawner : MonoBehaviour
+{
+    public SpawnManager spawnerScript;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+      
+            spawnerScript.GetComponent<SpawnManager>().InitializeSpawner();
+            Destroy(gameObject);
+
+        }
+
+    }
+}
