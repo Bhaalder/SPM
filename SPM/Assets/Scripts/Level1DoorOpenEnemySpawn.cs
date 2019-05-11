5aca901e5609d88f93f393653937e4bb353778f8
teoalagic <teoalagicenglund@hotmail.com>
2019-05-11 17:01:33 +0200

    hjort ändringar i scen 1

--- SPM/Assets/Scripts/Level1DoorOpenEnemySpawn.cs
+++ SPM/Assets/Scripts/Level1DoorOpenEnemySpawn.cs
@@ -6,31 +6,44 @@ public class Level1DoorOpenEnemySpawn : MonoBehaviour
 {
     //Author: Teo
     public GameObject aDoor;
+    public GameObject parent;
     public SpawnManager spawnerScript;
-    
+    bool isBlack;
+    Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rend = parent.GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if(other.gameObject.CompareTag("InteractionPlayer") && GameController.Instance.playerIsInteracting)
+        if (other.gameObject.CompareTag("InteractionPlayer") && GameController.Instance.playerIsInteracting)
         {
             Debug.Log("F");
-            aDoor.SetActive(false);
+
+            aDoor.SetActive(!aDoor.activeSelf);
+            isBlack = !isBlack;
             spawnerScript.GetComponent<SpawnManager>().InitializeSpawner();
             Destroy(gameObject);
+            if (isBlack)
+            {
+                rend.material.color = Color.red;
+            }
+            else
+            {
+                rend.material.color = Color.black;
+                
 
-            //if(hasSpawner)
+                //if(hasSpawner)
+            }
+
         }
-
     }
 }
