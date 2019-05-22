cafaf44b65f543ca4a181786c79f36b08ba80cce
Olsoo <37656328+Olsoo@users.noreply.github.com>
2019-05-23 00:39:41 +0200

    Update OpenDoorsAnimated.cs

--- SPM/Assets/Scripts/OpenDoorsAnimated.cs
+++ SPM/Assets/Scripts/OpenDoorsAnimated.cs
@@ -33,7 +33,6 @@ public class OpenDoorsAnimated : MonoBehaviour
             greenPanel.SetActive(!greenPanel.activeSelf);
             redPanel.SetActive(!redPanel.activeSelf);
             if (spawnEnemies) {
-                AudioController.Instance.PlayMusic("TestLoop");///////--------------------------------------
                 spawnManager.InitializeSpawner();
                 spawnEnemies = false;
             }
