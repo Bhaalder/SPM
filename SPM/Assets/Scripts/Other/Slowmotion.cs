4531c09e523de9ea1d9368a5dcea9cb71227044e
Olsoo <37656328+Olsoo@users.noreply.github.com>
2019-05-23 15:58:07 +0200

    Update Slowmotion.cs

--- SPM/Assets/Scripts/Other/Slowmotion.cs
+++ SPM/Assets/Scripts/Other/Slowmotion.cs
@@ -6,22 +6,29 @@ public class Slowmotion:MonoBehaviour{
     //Author: Patrik Ahlgren
 
     public float slowdownAmount;
+    private bool canSlowmo = true;
     
     public void SlowTime() {
         if (!GameController.Instance.gameIsPaused) {
-            if (!GameController.Instance.gameIsSlowmotion) {               
+            if (!GameController.Instance.gameIsSlowmotion && canSlowmo) {               
                 Time.timeScale = slowdownAmount;
                 AudioController.Instance.SFXSetPitch(0.5f);
+                GameController.Instance.gameIsSlowmotion = true;
             } else if (GameController.Instance.gameIsSlowmotion) {
                 Time.timeScale = 1f;
                 AudioController.Instance.SFXSetPitch(1f);
+                GameController.Instance.gameIsSlowmotion = false;
             }
             Time.fixedDeltaTime = Time.timeScale * 0.02f;
-            GameController.Instance.gameIsSlowmotion = !GameController.Instance.gameIsSlowmotion;
         }
     }
 
     private void Update() {
+        if (GameController.Instance.SlowmotionSlider.value == 100) {
+            canSlowmo = true;
+        } else {
+            canSlowmo = false;
+        }
         if (GameController.Instance.gameIsSlowmotion && GameController.Instance.SlowmotionSlider.value == 0) {
             GameController.Instance.gameIsSlowmotion = false;
             Time.timeScale = 1f;
@@ -34,9 +41,9 @@ public class Slowmotion:MonoBehaviour{
     private void SlowmotionSlider() {
         if (!GameController.Instance.gameIsPaused) {
             if (!GameController.Instance.gameIsSlowmotion && GameController.Instance.SlowmotionSlider.value < 100) {
-                GameController.Instance.SlowmotionSlider.value += 5 * Time.unscaledDeltaTime; //20sek
+                GameController.Instance.SlowmotionSlider.value += 10 * Time.unscaledDeltaTime; //10sek
             } else if (GameController.Instance.gameIsSlowmotion) {
-                GameController.Instance.SlowmotionSlider.value -= 10 * Time.unscaledDeltaTime; //10sek
+                GameController.Instance.SlowmotionSlider.value -= 20 * Time.unscaledDeltaTime; //5sek
             }
         }
     }
