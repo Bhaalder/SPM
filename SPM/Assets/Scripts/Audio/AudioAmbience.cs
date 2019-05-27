dab93eacd418453664d6cb2d3bb2bafd7528140b
teoalagic <teoalagicenglund@hotmail.com>
2019-05-27 18:16:27 +0200

    1

---
+++ SPM/Assets/Scripts/Audio/AudioAmbience.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioAmbience : MonoBehaviour
+{
+    public GameObject ting;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioController.Instance.Play_InWorldspace("Space Ambience",ting);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
