a90f82eba79dc3edf2c99e469467b7f07f0333e1
Olsoo <37656328+Olsoo@users.noreply.github.com>
2019-05-31 02:31:19 +0200

    asd

--- SPM/Assets/EnemyEventAudio.cs
+++ SPM/Assets/EnemyEventAudio.cs
@@ -2,26 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyEventAudio : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class EnemyEventAudio : MonoBehaviour{
 
-    // Update is called once per frame
-    void Update()
+    public void PlayEnemySound(string name)
     {
-        
-    }
-
-    public void Enemy4Roar()
-    {
-        AudioController.Instance.Play_InWorldspace("Enemy4Roar", gameObject);
-    }
-    public void Enemy4MeleeAttack()
-    {
-        AudioController.Instance.Play_InWorldspace("Enemy4MeleeAttack", gameObject);
+        AudioController.Instance.Play_InWorldspace(name, gameObject);
     }
 }
