e807bd33d280b30d6b96096f99f23a2ef73d1926
teoalagic <teoalagicenglund@hotmail.com>
2019-06-01 00:18:22 +0200

    TestSubjectsInLab

---
+++ SPM/Assets/ANimation/Animator/SubjectAnimations.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubjectAnimations : MonoBehaviour
+{
+    public Animator anim;
+
+    public  bool isSuperHuman;
+    public bool isAlien1;
+
+
+    void Start()
+    {
+        if (isSuperHuman)
+        {
+            anim.SetBool("SuperHuman", true);
+            AudioController.Instance.Play_InWorldspace_WithTag("LabBubbling", "SuperHuman");
+        }else if (isAlien1)
+        {
+            anim.SetBool("Alien1", true);
+
+        }
+    }
+
+    // Update is called once per frame
+    
+}
