90e60b548c61799722417fef6c72c80f94b590dd
Bhaalder <37620605+Bhaalder@users.noreply.github.com>
2019-05-31 12:59:30 +0200

    Update EnemyAttackTrigger.cs
    Deleted unneccessary debug info

--- SPM/Assets/Scripts/AI/EnemyAttackTrigger.cs
+++ SPM/Assets/Scripts/AI/EnemyAttackTrigger.cs
@@ -14,16 +14,10 @@ public class EnemyAttackTrigger : MonoBehaviour
         canDamage = true;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log("Current Animator State is: " + GetComponentInParent<Enemy>().CurrentAnimatorState("MeleeAttack"));
-    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player" && GetComponentInParent<Enemy>().CurrentAnimatorState("MeleeAttack") == true && canDamage == true)
         {
-            Debug.Log("Animation MeleeAttack is running");
             GetComponentInParent<Enemy>().DoMeleeDamage();
             StartCoroutine(TimeBetweeenDamage());
         }
