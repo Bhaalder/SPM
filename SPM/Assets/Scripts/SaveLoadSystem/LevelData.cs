5139550db4e5b0b6a3332142a63eb74421bd1a0a
Bhaalder <37620605+Bhaalder@users.noreply.github.com>
2019-06-03 00:45:05 +0200

    saveload

---
+++ SPM/Assets/Scripts/SaveLoadSystem/LevelData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class LevelData
+{
+
+    public float Timer { get; set; }
+     
+
+    public LevelData(DataStorage data)
+    {
+
+    }
+}
