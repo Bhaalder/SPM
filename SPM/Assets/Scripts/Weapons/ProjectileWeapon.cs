0fd626c0f137e3f0b1effe569073d1a5aa5d5fe1
Bhaalder <37620605+Bhaalder@users.noreply.github.com>
2019-06-03 12:05:24 +0200

    Update ProjectileWeapon.cs

--- SPM/Assets/Scripts/Weapons/ProjectileWeapon.cs
+++ SPM/Assets/Scripts/Weapons/ProjectileWeapon.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class ProjectileWeapon : BaseWeapon{
     //Author: Patrik Ahlgren
     private float projectileSpeed;
