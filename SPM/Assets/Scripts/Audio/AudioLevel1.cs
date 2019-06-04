a4f68b0b75c54c64cd07bd8e09ce2f9ca77da4f5
teoalagic <teoalagicenglund@hotmail.com>
2019-06-04 20:28:20 +0200

    Ljud

--- SPM/Assets/Scripts/Audio/AudioLevel1.cs
+++ SPM/Assets/Scripts/Audio/AudioLevel1.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 
 public class AudioLevel1 : MonoBehaviour{
-
+    [SerializeField] bool barSign;
     void Start(){
         //AudioController.Instance.Play("RandomAmbience");
         AudioController.Instance.Play_InWorldspace_WithTag("LabBubbling", "SuperHuman");
+
+        if (barSign)
+        {
+            AudioController.Instance.Play_InWorldspace("BarSign", gameObject);
+            AudioController.Instance.Play_InWorldspace("BarSignFlick", gameObject);
+        }
+
     }
     private void Update()
     {
