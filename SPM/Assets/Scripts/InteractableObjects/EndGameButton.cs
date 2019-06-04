c6a290bdc0055464a1c384f5f881231dac99b269
Olsoo <37656328+Olsoo@users.noreply.github.com>
2019-06-04 06:30:14 +0200

    endgame stuff

---
+++ SPM/Assets/Scripts/InteractableObjects/EndGameButton.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameButton : MonoBehaviour {
+    //Author: Patrik Ahlgren
+
+    [SerializeField] private GameObject timerGO;
+    private Timer timer;
+
+    private void Start() {
+        timerGO = GameController.Instance.GetComponent<Timer>().GetTimerObject();
+        timer = timerGO.GetComponent<Timer>();
+    }
+
+    public void PressButton() {
+        timer.TimerIsActive = false;
+        timerGO.transform.SetAsLastSibling();
+
+    }
+
+    private IEnumerator ExplosionEnding() {
+        AudioController.Instance.Play_Delay("Explosion", 0.3f, 2.7f);
+        
+        AudioController.Instance.FadeOut("Explosion", 5, 0);
+        AudioController.Instance.StopAllSounds();
+        //Skärmen fadear
+
+
+        yield return null;
+    }
+
+//    ENDGAME
+
+//Dörren öppnas till sista utrymmet
+//AudioController drar igång AlarmLjud vid knappen
+//Tip: Press the button to save humanity!
+
+//Knappen går att trycka på/dyker upp(tydlig placering)
+
+//Klicka på knappen
+//Timern stannar(set as LastSibling)
+//Skärmen fadear
+//Skärmen skakar
+//Explosionljud
+//Allt ljud stoppas
+//5sec
+
+//Svart skärm med Score
+//Score är tid och killcount
+
+//Killcount +10 i Score per kill
+
+//TimeBonus +1 per sec under 30min
+
+
+//1800sec = 30min
+//900sec
+
+//30minfloat - totalTimefloat = Timescore
+
+//Timer: 15:31:02
+//Kills: 20
+
+//SCORE: 581! (räknar upp)
+}
---
+++ SPM/Assets/Scripts/Other/ScoreScreen.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScreen : MonoBehaviour{
+    //Author: Patrik Ahlgren
+    [SerializeField] private Text killCountText;
+
+    [SerializeField] private Text timeScoreText;
+    [SerializeField] private Text killScoreText;
+
+    [SerializeField] private Text totalScoreText;
+
+
+    private int maxTimerScore = 1800;
+
+    private int timeScore = 0;
+    private int killScore = 0;
+    private int totalScore = 0;
+
+    private bool isEndScreen;
+
+    private void Update() {
+        if (isEndScreen) {
+            totalScoreText.text = totalScore.ToString();
+            timeScoreText.text = timeScore.ToString();
+        }
+    }
+
+    public void CountScore(float totalTime, float killCount) {
+        isEndScreen = true;
+        StartCoroutine(Counter(totalTime, killCount));
+    }
+
+    private IEnumerator Counter(float totalTime, float killCount) {
+        int time = Mathf.RoundToInt(totalTime);
+        for(int i = time; i < maxTimerScore; i++) {
+            timeScore++;
+            totalScore++;
+            yield return new WaitForSeconds(0.03f);
+        }
+        for(int i = 0; i < killCount; i++) {
+            killScore += 10;
+            totalScore += 10;
+            yield return new WaitForSeconds(0.03f);
+        }
+
+        yield return null;
+    }
+
+}
+//Svart skärm med Score
+//Score är tid och killcount
+
+//Killcount +10 i Score per kill
+
+//TimeBonus +1 per sec under 30min
+
+
+//1800sec = 30min
+//900sec
+
+//30minfloat - totalTimefloat = Timescore
+
+//Timer: 15:31:02
+//Kills: 20
+
+//SCORE: 581! (räknar upp)
