da957b16701590647bc46a3bae57b84e51401dcb
teoalagic <teoalagicenglund@hotmail.com>
2019-06-05 01:02:29 +0200

    Revert "Yikes"
    This reverts commit efe9221cff7e905d83306cf79054dae6abca9369.

--- SPM/Assets/Scripts/Enemy/TriggerEnemySpawner.cs
+++ SPM/Assets/Scripts/Enemy/TriggerEnemySpawner.cs
@@ -5,62 +5,18 @@ using UnityEngine;
 public class TriggerEnemySpawner : MonoBehaviour
 {
     //Author: Teo
-    public SpawnManager Hangarspawner;
-    public SpawnManager Storagespawner;
-    public SpawnManager hexagonspawner;
-    public GameObject hangarDoor;
+    public SpawnManager spawnerScript;
 
-    [SerializeField] bool hangar;
-    [SerializeField] bool storage;
-    [SerializeField] bool hexagon;
-
-    private bool triggered;
-    //public GameObject storageDoor;
 
     private void OnTriggerEnter(Collider other)
     {
-
-
-
         if (other.gameObject.CompareTag("InteractionPlayer"))
         {
-
-
-            if (hangar && !triggered)
-            {
-
-                WaitASecHangar();
-                hangarDoor.SetActive(true);
-                triggered = true;
-            }
-
-            if (storage && !triggered)
-            {
-                WaitASecStorage();
-                triggered = true;
-            }
-            if(hexagon && !triggered)
-            {
-                hexagonspawner.InitializeSpawner();
-                triggered = true;
-            }
-
-                Destroy(gameObject);
+      
+            spawnerScript.GetComponent<SpawnManager>().InitializeSpawner();
+            Destroy(gameObject);
 
         }
 
     }
-    private IEnumerator WaitASecHangar()
-    {
-        yield return new WaitForSeconds(2);
-        AudioController.Instance.Play_ThenPlay("Song2Start", "Song2Loop");
-        Hangarspawner.InitializeSpawner();
-    }
-    private IEnumerator WaitASecStorage()
-    {
-        yield return new WaitForSeconds(2);
-        AudioController.Instance.Play_ThenPlay("Song2Start", "Song2Loop");
-        Storagespawner.InitializeSpawner();
-    }
 }
-
