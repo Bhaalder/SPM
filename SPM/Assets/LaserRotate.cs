747e16a19499090c12c2bea9eeb00ec228fa1189
teoalagic <teoalagicenglund@hotmail.com>
2020-03-24 15:01:05 +0100

    laser

---
+++ SPM/Assets/LaserRotate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserRotate : MonoBehaviour
+{
+    public float speed = 20f;
+    
+    
+
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.Rotate(Vector3.up * Time.deltaTime * speed);
+    }
+}
